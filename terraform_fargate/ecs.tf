a50bd1273375bfdfad5b450f572928e31e33b7f2
Aadith <109966558+Aadith-tech@users.noreply.github.com>
2025-12-16 11:44:57 +0530

    Update ecs.tf

--- terraform_fargate/ecs.tf
+++ terraform_fargate/ecs.tf
@@ -3,10 +3,10 @@
 resource "aws_ecs_cluster" "strapi_cluster" {
   name = "aadith-strapi-cluster"
 
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
+  # setting {
+  #   name  = "containerInsights"
+  #   value = "enabled"
+  # }
 
   tags = {
     Name = "aadith-strapi-cluster"
@@ -31,7 +31,7 @@ resource "aws_ecs_task_definition" "strapi_task" {
   cpu                      = "2048"  # 2 vCPU
   memory                   = "4096"  # 4 GB
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  #task_role_arn            = aws_iam_role.ecs_task_role.arn
 
   container_definitions = jsonencode([
     {
@@ -160,46 +160,46 @@ resource "aws_ecs_service" "strapi_service" {
 }
 
 # Auto Scaling Target
-resource "aws_appautoscaling_target" "ecs_target" {
-  max_capacity       = 3
-  min_capacity       = 1
-  resource_id        = "service/${aws_ecs_cluster.strapi_cluster.name}/${aws_ecs_service.strapi_service.name}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
-}
+# resource "aws_appautoscaling_target" "ecs_target" {
+#   max_capacity       = 3
+#   min_capacity       = 1
+#   resource_id        = "service/${aws_ecs_cluster.strapi_cluster.name}/${aws_ecs_service.strapi_service.name}"
+#   scalable_dimension = "ecs:service:DesiredCount"
+#   service_namespace  = "ecs"
+# }
 
 # Auto Scaling Policy - CPU Based
-resource "aws_appautoscaling_policy" "ecs_cpu_policy" {
-  name               = "aadith-strapi-cpu-autoscaling"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization"
-    }
-    target_value       = 70.0
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 60
-  }
-}
+# resource "aws_appautoscaling_policy" "ecs_cpu_policy" {
+#   name               = "aadith-strapi-cpu-autoscaling"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+#   scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+# 
+#   target_tracking_scaling_policy_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageCPUUtilization"
+#     }
+#     target_value       = 70.0
+#     scale_in_cooldown  = 300
+#     scale_out_cooldown = 60
+#   }
+# }
 
 # Auto Scaling Policy - Memory Based
-resource "aws_appautoscaling_policy" "ecs_memory_policy" {
-  name               = "aadith-strapi-memory-autoscaling"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-    target_value       = 80.0
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 60
-  }
-}
+# resource "aws_appautoscaling_policy" "ecs_memory_policy" {
+#   name               = "aadith-strapi-memory-autoscaling"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+#   scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+# 
+#   target_tracking_scaling_policy_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+#     }
+#     target_value       = 80.0
+#     scale_in_cooldown  = 300
+#     scale_out_cooldown = 60
+#   }
+# }
